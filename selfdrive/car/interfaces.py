82eae77fee04296733f29243eeecaf7fbc1e4ed5
chobyounghak <61299886+chobyounghak@users.noreply.github.com>
2020-06-12 19:06:19 +0900

    dd

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -129,9 +129,9 @@ class CarInterfaceBase():
     #  elif not cs_out.cruiseState.enabled:
     #    events.add(EventName.pcmDisable)
 
-    if events != None:
-      self.cruise_enabled_prev = False
-    elif pcm_enable:
+    #if events != None:
+    #  self.cruise_enabled_prev = False
+    if pcm_enable:
       if cs_out.cruiseState.enabled != self.cruise_enabled_prev:
         if cs_out.cruiseState.enabled:
           events.add(EventName.pcmEnable)
