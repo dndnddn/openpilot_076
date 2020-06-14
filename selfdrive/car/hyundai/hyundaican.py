77cd4317af5929fef72574a79fcf091db59c80f4
chobyounghak <61299886+chobyounghak@users.noreply.github.com>
2020-06-14 17:20:50 +0900

    dd

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -108,8 +108,8 @@ def create_scc12(packer, apply_accel, enabled, cnt, scc12):
   
 def create_mdps12(packer, frame, mdps12):
   values = mdps12
-  values["CF_Mdps_ToiActive"] = 0
-  values["CF_Mdps_ToiUnavail"] = 0 #1
+  #values["CF_Mdps_ToiActive"] = 0
+  #values["CF_Mdps_ToiUnavail"] = 1
   values["CF_Mdps_MsgCount2"] = frame % 0x100
   values["CF_Mdps_Chksum2"] = 0
 
