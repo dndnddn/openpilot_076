9c53adad0c2538080110e746805a3edd5c9c8f0e
chobyounghak <61299886+chobyounghak@users.noreply.github.com>
2020-06-15 23:33:55 +0900

    dd

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -49,7 +49,7 @@ class CarInterface(CarInterfaceBase):
 
 
       ret.steerRatio = 10.5  #12.5
-      ret.steerRateCost = 0.5 #0.4
+      ret.steerRateCost = 0.4 #0.4
       ret.lateralTuning.pid.kf = 0.00001
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[9., 22.], [9., 22.]]
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.15, 0.18], [0.02, 0.03]]
