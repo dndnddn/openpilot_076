4ad273dc1fa9df0733ede26a8ab2d58568a19d13
chobyounghak <61299886+chobyounghak@users.noreply.github.com>
2020-06-17 16:52:46 +0900

    dd

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -16,7 +16,7 @@ class CarInterface(CarInterfaceBase):
 
     self.meg_timer = 0
     self.meg_name = 0
-"""
+  """
     self.steer_Kf1 = [0.00001,0.000015]    
     self.steer_Ki1 = [0.01,0.01]
     self.steer_Kp1 = [0.11,0.12]
